eb8523afa0713906a90e53b837c903cfa29a842a
Johnathan Andrés León Rodriguez <Johnathan.Leon@ibm.com>
2020-11-23 18:42:05 -0500

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({dish}) {
@@ -20,20 +21,22 @@ function RenderDish({dish}) {
         );
 }
 
-function RenderComments({dish}) {
-    if (dish != null) {
-        const comments = dish.comments.map((comment) => {
-            return (
-            <li key={comment.id}><br></br>{comment.comment}<br></br>-- {comment.author}, --
-            {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
-            .format(new Date(Date.parse(comment.date)))}</li>
-            );
-        });
+function RenderComments({comments}) {
+    if (comments != null) {        
         return(
             <div>
-                <h3>Comments</h3>
+                <h4>Comments</h4>
                 <ul className="list-unstyled">
-                    {comments}
+                    {comments = comments.map((comment) => {
+                    return (
+                    <li key={comment.id}>
+                        <p>{comment.comment}</p>
+                        <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'})
+                        .format(new Date(Date.parse(comment.date)))}
+                        </p>
+                    </li>
+                    );
+        })}
                 </ul>
             </div>
         );
@@ -47,15 +50,25 @@ function RenderComments({dish}) {
 const  DishDetail = (props) => {
     return (
         <div className="container">
-            <div className="row">
-                <div  className="col-12 col-md-5 m-1">
-                    <RenderDish dish={props.dish}/>
-                </div>
-                <div  className="col-12 col-md-5 m-1">                
-                    <RenderComments dish={props.dish}/>
-                </div>
+        <div className="row">
+            <Breadcrumb>
+                <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+                <h3>{props.dish.name}</h3>
+                <hr />
+            </div>                
+        </div>
+        <div className="row">
+            <div className="col-12 col-md-5 m-1">
+                <RenderDish dish={props.dish} />
+            </div>
+            <div className="col-12 col-md-5 m-1">
+                <RenderComments comments={props.comments} />
             </div>
         </div>
-        );
+        </div>
+    );
 }
 export default DishDetail;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -33,12 +34,20 @@ class Main extends Component {
       );
     }
 
+    const DishWithId = ({match}) => {
+      return(
+          <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+
     return (
       <div>
         <Header />
         <Switch>
               <Route path='/home' component={HomePage} />
               <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+              <Route path='/menu/:dishId' component={DishWithId} />
               <Route exact path='/contactus' component={Contact} />
               <Redirect to="/home" />
         </Switch>
